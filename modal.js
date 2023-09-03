1502cd7dccb623fed0f33c5bdb486bf7f2438527
Irakli Akhvlediani <irakliakhvlediani@hotmail.com>
2023-09-03 05:55:44 +0400

    final

--- modal.js
+++ modal.js
@@ -1,3 +1,24 @@
+async function getUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const parsedData = await response.json();
+    const html = '';
+
+    parsedData.forEach((user) => {
+        const userHtml = `
+        <div class="user">
+        <h2>${user.name}</h2>
+        <p>${user.email}</p>
+        </div>
+        `;
+
+        html += userHtml;
+    })
+
+    const userDiv = document.getElementById('modal');
+    userDiv.innerHTML = html;
+}
+
+
 
             const modal = document.querySelector('#modal');
             const openModal = document.querySelector('.open-button');
