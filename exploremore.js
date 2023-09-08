7df5e2556726c7697a5ce197920379b380e968cc
Irakli Akhvlediani <irakliakhvlediani@hotmail.com>
2023-09-09 03:16:37 +0400

    finalwrapper

--- exploremore.js
+++ exploremore.js
@@ -1,12 +1,34 @@
+        
             const modl = document.querySelector('#modl');
             const openModl = document.querySelector('.open');
             const closeModl = document.querySelector('.close');
 
             openModl.addEventListener('click', () => {
-                modl.showModal();
-
+                modl.showModal(getUsers);
+                // აქ უნდა გამოვიძახო getUsers და ვერ მოვაბი თავი ვერანაირად//
+                         
                 
             })
+
+            async function getUsers() {
+            const response =await fetch ('https://jsonplaceholder.typicode.com/users');
+            const parsedData = await response.json();
+            const html = '';
+
+            parsedData.forEach((user) => {
+                const userHtml = `
+                <dialog class="modl">
+                    <h4>${user.name}</h4>
+                    <h5>${user.email}</h5>
+                </dialog>`;
+
+                html += userHtml;
+            });
+
+            const userDialog = document.getElementById('modl');
+            userDialog.innerHTML = html;
+            }
+
 
             closeModl.addEventListener('click', () => {
                 modl.close();
--- modal.js
+++ modal.js
@@ -1,37 +1,31 @@
-   
-   
-//    async function getUsers() {
-//    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-//    const parsedData = await response.json();
-//    const html = '';
-
-//     parsedData.forEach((user) => {
-//        const userHtml = `
-//        <div class="user">
-//         <h2>${user.name}</h2>
-//         <p>${user.email}</p>
-//         </div>
-//         `;
-
-//         html += userHtml;
-//     })
-
-// //     const userDiv = document.getElementById('modal');
-// //     userDiv.innerHTML = html;
-//  }
-
-
 
             const modal = document.querySelector('#modal');
             const openModal = document.querySelector('.open-button');
             const closeModal = document.querySelector('.close-button');
 
             openModal.addEventListener('click', () => {
-                modal.showModal();
-                
-
-                
+                modal.showModal();   
             })
+
+
+            async function getTodos() {
+                const response =await fetch ('https://jsonplaceholder.typicode.com/todos');
+                const parsedData = await response.json();
+                const html = '';
+    
+                parsedData.forEach((user) => {
+                    const userHtml = `
+                    <dialog class="modal">
+                        <h4>${user.id}</h4>
+                        <h4>${user.title}</h4>
+                    </dialog>`;
+    
+                    html += userHtml;
+                });
+    
+                const userDialog = document.getElementById('modal');
+                userDialog.innerHTML = html;
+                }
 
             closeModal.addEventListener('click', () => {
                 modal.close();
